0efce0742ae65c9cfc8bd542072f1c56f15ba629
Coordinate-Cat <tetrapasta02@gmail.com>
2022-12-03 23:54:59 +0900

    feat: motley

--- src/pages/socialMedia/index.tsx
+++ src/pages/socialMedia/index.tsx
@@ -1,9 +1,9 @@
 import type { NextPage } from "next"
 import Link from "next/link"
 
-const About: NextPage = () => {
+const SocialMedia: NextPage = () => {
   return (
-    <div className="ml-4 flex flex-1 flex-col items-start py-4">
+    <div className="ml-4 flex flex-1 flex-col items-start bg-black py-4 text-white">
       <nav className="flex">
         <Link
           className="w-fit rounded-sm border-2 border-dashed border-white bg-black px-2 py-1 text-white duration-500 ease-out hover:bg-white hover:text-black"
@@ -12,10 +12,19 @@ const About: NextPage = () => {
           ←home
         </Link>
         <h1 className="ml-2 w-fit rounded-md border-2 border-solid border-black px-2 py-1 text-white">
-          About Page
+          Social Media Page
         </h1>
       </nav>
+
+      <div className="database">
+        <div>Youtube</div>
+        <div>Twitter</div>
+        <div>Reddit</div>
+        <div>Facebook</div>
+        <div>Pinterest</div>
+        <div>Instagram</div>
+      </div>
     </div>
   )
 }
-export default About
+export default SocialMedia
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontSize: {
+      'jb': ['9.875rem', {
+        lineHeight: '9.25rem',
+        letterSpacing: '-0.02em',
+        fontWeight: '700',
+      }],
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    }
   },
   plugins: [],
 }
